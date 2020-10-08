1e6119e56b258ec5c43004333c967ed29becf9e7
wethemel <56676545+wethemel@users.noreply.github.com>
2020-10-08 11:27:42 +0200

    Made AlphaPose importer

--- apiir/testfile.py
+++ apiir/testfile.py
@@ -1,8 +1,10 @@
 import alphapose_tools.alphapose_results_importer as ari
-# test = ari.json_interface('10frames_test.json')
+
+
+
 test2 = ari.json_interface('10frames_test.json')
-# test3 = ari.json_interface("APIIR\apiir\alphapose_tools\10frames_test.json")
 
 test2.COCO_load_to_dict()
+
 frame1 = test2.GetFrames()[1]
-print(frame1.Keypoints(), frame1.FrameID())
+print(frame1.Keypoints()['LKnee'], frame1.FrameID())
